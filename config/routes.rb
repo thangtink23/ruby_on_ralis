a5b3d548b0710502265842c98af284ec7bc1a7eb
thangtink23 <thangtink23@gmail.com>
2017-12-15 15:47:11 +0700

    add_view_home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # bookmarks 
+  get 'bookmarks', to: 'bookmarks#index'
+  post 'bookmarks', to: 'bookmarks#create'
+  delete 'bookmarks', to: 'bookmarks#destroy'
+
+  root to: 'pages#home'
+
 end
