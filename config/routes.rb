c9b39f81f5d8a8286ddde978a350b5ae52d2e3be
GIOsBK <hatran10091996@gmail.com>
2017-12-15 16:36:03 +0700

    add_profiles

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
 
 
   devise_for :users
+get "user/:id", to: 'profiles#show', as: 'user_profile'
+  post "avatar/:id", to: 'profiles#avatar', as: 'user_profile_avatar'
+  get 'profile/change_password', to: 'profiles#change_password', as: "user_change_password"
+  patch "profile/change_password", to: 'profiles#update_password', as: 'user_update_password'
+  
   resources :hotels
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # bookmarks 
